825f65b3fc5f942f2af1ce00695bf25148832b3e
Manoruo <manoruo1@umbc.edu>
2023-03-18 23:16:03 -0400

    Code for HW2 with backend of website working and react being setup on the front end

---
+++ HW2/backend/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request 
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+app = Flask(__name__)
+    
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:cartoon12@localhost/HW_DB'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    points = db.Column(db.Integer)
+
+    def __init__(self, name, points):
+        self.name = name
+        self.points = points 
+
+def format_user(user):
+    return {
+        "id": user.id,
+        "points": user.points,
+        "name": user.name 
+    }
+    
+        
+@app.route("/")   
+def home():
+    return "Hello, Flask!"
+
+# create a new user 
+@app.route('/user', methods=['POST'])
+def create_user():
+    name = request.json['name']
+    points = request.json['points']
+    user = User(name, points)
+    db.session.add(user)
+    db.session.commit()
+    return format_user(user)
+
+
+# serach multiple users 
+@app.route("/users", methods = ["GET"])
+def get_users():
+    users = User.query.order_by(User.id.asc()).all()
+    user_list  = []
+    for user in users:
+        user_list.append(format_user(user))
+    return {'users': user_list}
+
+# Search single user 
+@app.route("/users/<id>", methods = ["GET"])
+def get_user(id):
+    user = User.query.filter_by(id=id).one()
+    formatted_user = format_user(user)
+    return {'user': formatted_user}
+
+
+# delete user 
+@app.route('/users/<id>', methods = ['DELETE'])
+def delete_user(id):
+    user = User.query.filter_by(id=id).one()
+    db.session.delete(user)
+    db.session.commit() 
+    return f'Event (id: {id}) deleted'
+
+if __name__ == '__main__':
+    app.run()
